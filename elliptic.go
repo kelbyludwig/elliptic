977a6ade04ea4c543d0fb053657951c66b28f3f6
Kelby Ludwig <kelbyludwig@gmail.com>
2015-12-19 18:28:12 -0600

    Pointer chasing was obnoxious. Removed all method calls and made functions on types

--- elliptic.go
+++ elliptic.go
@@ -1,6 +1,14 @@
 package elliptic
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+)
+
+var debugAdd bool = false
+var debugScalarMult bool = true
+var debugSet bool = false
+var debugInverse bool = false
 
 //Curve represents an equation of the form: y^2 = x^3 + A*x + B
 type Curve struct {
@@ -35,7 +43,9 @@ func (c *Curve) equals(ci *Curve) bool {
 
 //NewPoint initializes a new non-identity point that (maybe) lies on Curve.
 func NewPoint(x, y *big.Int, curve *Curve) *Point {
-	p := &Point{X: x, Y: y, Nonzero: true, Curve: curve}
+	p := &Point{Nonzero: true, Curve: curve}
+	p.X = new(big.Int).Set(x)
+	p.Y = new(big.Int).Set(y)
 	return p
 }
 
@@ -43,31 +53,53 @@ func NewPoint(x, y *big.Int, curve *Curve) *Point {
 func Identity(c *Curve) *Point {
 	p := new(Point)
 	p.Curve = c
+	p.X = new(big.Int)
+	p.Y = new(big.Int)
 	return p
 }
 
 //Add sets p to the result of p1 + p2 and returns p
-func (p *Point) Add(p1, p2 *Point) *Point {
+func Add(p1, p2 *Point) *Point {
+
+	if debugAdd {
+		fmt.Printf("[ADD] POINT ADDITION: (%v,%v) + (%v,%v)\n", p1.X, p1.Y, p2.X, p2.Y)
+		fmt.Printf("[ADD] Original Addrs: &p1.X: %v - &p1.Y: %v\n", &p1.X, &p1.Y)
+		fmt.Printf("[ADD] Original Addrs: &p2.X: %v - &p2.Y: %v\n", &p2.X, &p2.Y)
+	}
 
 	if p1.IsIdentity() {
-		return p.Set(p2)
+		if debugAdd {
+			fmt.Printf("[ADD] p1 is identity\n")
+		}
+		return new(Point).Set(p2)
 	}
 
 	if p2.IsIdentity() {
-		return p.Set(p1)
+		if debugAdd {
+			fmt.Printf("[ADD] p2 is identity\n")
+		}
+		return new(Point).Set(p1)
 	}
 
-	inverse := new(Point)
-	inverse.Set(p2)
-	inverse.Inverse(inverse)
+	if debugAdd {
+		fmt.Printf("[ADD] Setting up p2's inverse\n")
+	}
+	inverse := new(Point).Set(p2)
+	inverse = Inverse(inverse)
 
 	if p1.Equals(inverse) {
+		if debugAdd {
+			fmt.Printf("[ADD] p1 is equal to the inverse of p2\n")
+		}
 		id := Identity(p1.Curve)
-		return p.Set(id)
+		return id
 	}
 
 	m := big.NewInt(0)
 	if p1.Equals(p2) {
+		if debugAdd {
+			fmt.Printf("[ADD] p1 is equal to p2\n")
+		}
 		//This is just an ugly way of saying: m := (3 * p1.X ^ 2 + a) / 2 * p1.Y
 		m.Exp(p1.X, big.NewInt(2), p1.Curve.P)
 		m.Mul(m, big.NewInt(3))
@@ -76,6 +108,9 @@ func (p *Point) Add(p1, p2 *Point) *Point {
 		rhs.Mul(big.NewInt(2), p1.Y)
 		m.Div(m, rhs)
 	} else {
+		if debugAdd {
+			fmt.Printf("[ADD] p1 is not equal to p2\n")
+		}
 		//This is just an ugly way of saying: m := (p2.Y - p1.Y) / (p2.X - p1.X)
 		m.Sub(p2.Y, p1.Y)
 		rhs := big.NewInt(0)
@@ -90,14 +125,38 @@ func (p *Point) Add(p1, p2 *Point) *Point {
 	x.Exp(m, big.NewInt(2), p1.Curve.P)
 	x.Sub(x, p1.X)
 	x.Sub(x, p2.X)
+	x.Mod(x, p1.Curve.P)
 
 	//This is an ugly way of saying: y := m * (p1.X - x) - p1.Y
 	y.Sub(p1.X, x)
 	y.Mul(m, y)
 	y.Sub(y, p1.Y)
+	y.Mod(y, p1.Curve.P)
 
 	res := NewPoint(x, y, p1.Curve)
-	return p.Set(res)
+	if debugAdd {
+		fmt.Printf("[ADD] POINT ADDITION: (%v,%v) + (%v,%v) = (%v, %v)\n", p1.X, p1.Y, p2.X, p2.Y, res.X, res.Y)
+	}
+	return res
+}
+
+//ScalarMult sets p to k*pi and returns p
+func ScalarMult(pi *Point, k *big.Int) *Point {
+	if debugScalarMult {
+		fmt.Printf("[MULT] SCALAR MULT: %v * (%v, %v)\n", k, pi.X, pi.Y)
+	}
+	p := new(Point).Set(pi)
+	for i := big.NewInt(0); i.Cmp(k) != 0; i.Add(i, big.NewInt(1)) {
+		if debugScalarMult {
+			fmt.Printf("[MULT] %v of %v: ", new(big.Int).Add(i, big.NewInt(1)), k)
+			fmt.Printf("p:(%v, %v) + pi:(%v, %v) = ", p.X, p.Y, pi.X, pi.Y)
+		}
+		p = Add(p, pi)
+		if debugScalarMult {
+			fmt.Printf("(%v, %v)\n", p.X, p.Y)
+		}
+	}
+	return p
 }
 
 //IsIdentity returns true if p is the identity point.
@@ -116,23 +175,31 @@ func (p *Point) Equals(pi *Point) bool {
 	return false
 }
 
-//Inverse sets p to pi's inverse and returns p
-func (p *Point) Inverse(pi *Point) *Point {
-	p.Set(pi)
-	p.Y.Neg(pi.Y)
-	p.Y.Mod(pi.Y, pi.Curve.P)
+//Inverse returns pi's inverse
+func Inverse(pi *Point) *Point {
+	ny := new(big.Int).Neg(pi.Y)
+	p := new(Point).Set(pi)
+	p.Y = ny
 	return p
 }
 
 //Set sets p to the value pi and returns p. This clones the bigints withitn Point.
 func (p *Point) Set(pi *Point) *Point {
-	x := *(pi.X)
-	y := *(pi.Y)
-	n := pi.Nonzero
-	c := pi.Curve
-	p.X = &x
-	p.Y = &y
-	p.Nonzero = n
-	p.Curve = c
+	if debugSet {
+		fmt.Printf("[SET] Setting p to pi = (%v, %v)\n", pi.X, pi.Y)
+		fmt.Printf("[SET] &p.X: %v - &pi.X: %v\n", &p, &pi)
+	}
+	p.X = new(big.Int).Set(pi.X)
+	p.Y = new(big.Int).Set(pi.Y)
+	p.Nonzero = pi.Nonzero
+	p.Curve = pi.Curve
+
+	if debugSet {
+		fmt.Printf("[SET] pi.X after set: %v\n", pi.X)
+		fmt.Printf("[SET] pi.Y after set: %v\n", pi.Y)
+		fmt.Printf("[SET] result point: (%v, %v)\n", p.X, p.Y)
+		fmt.Printf("[SET] &result.X: %v - &pi.X: %v\n", &(p.X), &(pi.X))
+		fmt.Printf("[SET] &result.Y: %v - &pi.Y: %v\n", &(p.Y), &(pi.Y))
+	}
 	return p
 }
--- elliptic_test.go
+++ elliptic_test.go
@@ -1,16 +1,37 @@
 package elliptic
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 )
 
-func TestAddition(t *testing.T) {
-	curve := NewCurve(big.NewInt(-1), big.NewInt(4), big.NewInt(65535))
-	x := NewPoint(big.NewInt(0), big.NewInt(2), curve)
-	y := NewPoint(big.NewInt(-1), big.NewInt(-2), curve)
-	x.Add(x, y)
-	if x.X.Cmp(big.NewInt(17)) != 0 || x.Y.Cmp(big.NewInt(-70)) != 0 {
-		t.Errorf("Addition test failed.\n")
+func TestSet(t *testing.T) {
+	curve := NewCurve(big.NewInt(9), big.NewInt(17), big.NewInt(23))
+	x := NewPoint(big.NewInt(1), big.NewInt(18), curve)
+	y := NewPoint(big.NewInt(11), big.NewInt(13), curve)
+	ybefore := fmt.Sprintf("(%v,%v)", y.X, y.Y)
+	x.Set(y)
+	xafter := fmt.Sprintf("(%v,%v)", x.X, x.Y)
+	yafter := fmt.Sprintf("(%v,%v)", y.X, y.Y)
+	if xafter != ybefore || ybefore != yafter {
+		t.Errorf("TestSet failed!\n")
 	}
+}
+
+func TestAddition(t *testing.T) {
+	curve := NewCurve(big.NewInt(9), big.NewInt(17), big.NewInt(23))
+	x := NewPoint(big.NewInt(16), big.NewInt(5), curve)
+	y := NewPoint(big.NewInt(16), big.NewInt(5), curve)
+	z := Add(x, y)
+	t.Errorf("TestAddition: (%v,%v)\n", z.X, z.Y)
+}
+
+func TestScalarMult(t *testing.T) {
+	curve := NewCurve(big.NewInt(9), big.NewInt(17), big.NewInt(23))
+	x := NewPoint(big.NewInt(16), big.NewInt(5), curve)
+	fmt.Printf("Original p : (%v, %v)\n", x.X, x.Y)
+	x = ScalarMult(x, big.NewInt(2))
+	fmt.Printf("2p: (%v, %v)\n", x.X, x.Y)
+	t.Errorf("DONE\n")
 }
